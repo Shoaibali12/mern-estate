5ca9c732bea2224e13fdfb724daa6716ee9fd747
Shoaibali12 <sind98449@gmail.com>
2025-01-14 22:52:36 -0800

    installed react js and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-800">Hellow world</div>
+    </>
+  );
+}
+
+export default App;
