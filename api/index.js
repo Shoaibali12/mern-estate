ade0bb9ac53e11d48846f42817729cc823b8a475
Shoaibali12 <sind98449@gmail.com>
2025-01-17 16:38:46 -0800

    Connect MongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const uri = "mongodb://localhost:27017";
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log("Error:", err);
+  });
 
 const app = express();
 
