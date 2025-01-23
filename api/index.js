f25d2c1f7f54b933a0cc9e1d9a3ee8e38b016c00
Shoaibali12 <sind98449@gmail.com>
2025-01-23 10:10:34 -0800

    create update user api

--- api/index.js
+++ api/index.js
@@ -2,26 +2,27 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js"; // Import user routes
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 const uri = "mongodb://localhost:27017";
 mongoose
   .connect(uri)
   .then(() => {
-    console.log("connected");
+    console.log("MongoDB connected");
   })
   .catch((err) => {
-    console.log("Error:", err);
+    console.error("Error connecting to MongoDB:", err);
   });
 
 const app = express();
 app.use(express.json());
-
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
-});
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter); // Register user routes
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -31,4 +32,8 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
